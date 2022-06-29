ebb00fd0759864593fd3b3de85b9254cecb9e5bb
康良玉 <kangly@vigamemedia.com>
2022-06-29 15:01:29 +0800

    添加Zuul网关服务

---
+++ ad-gateway/src/main/java/com/caogen/ad/ZuulGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.caogen.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-06-29 14:54
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
