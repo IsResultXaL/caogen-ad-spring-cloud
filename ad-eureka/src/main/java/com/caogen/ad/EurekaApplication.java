9e103899b43fcd6108986107563755b8fae07592
康良玉 <kangly@vigamemedia.com>
2022-06-29 11:28:38 +0800

    创建单节点Eureka注册中心

---
+++ ad-eureka/src/main/java/com/caogen/ad/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.caogen.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-06-29 00:51
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
