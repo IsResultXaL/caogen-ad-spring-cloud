926d9fcb8bb7adad7e1682d14f0a6ad32121c2de
康良玉 <kangly@vigamemedia.com>
2022-06-30 11:46:12 +0800

    数据表实体类定义

---
+++ caogen-ad-service/ad-sponsor/src/main/java/com/caogen/ad/entity/unit_condition/AdUnitDistrict.java
@@ -0,0 +1,43 @@
+package com.caogen.ad.entity.unit_condition;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-06-30 11:38
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ad_unit_district")
+public class AdUnitDistrict {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic
+    @Column(name = "unit_id", nullable = false)
+    private Long unitId;
+
+    @Basic
+    @Column(name = "province", nullable = false)
+    private String province;
+
+    @Basic
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    public AdUnitDistrict(Long unitId, String province, String city) {
+        this.unitId = unitId;
+        this.province = province;
+        this.city = city;
+    }
+}
