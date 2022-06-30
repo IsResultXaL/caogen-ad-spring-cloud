641ef8c51ae9318437bc119fde26ca0be22677bb
康良玉 <kangly@vigamemedia.com>
2022-06-30 11:27:10 +0800

    添加投放系统ad-sponsor服务模块

---
+++ caogen-ad-service/ad-sponsor/src/main/java/com/caogen/ad/SponsorApplication.java
@@ -0,0 +1,24 @@
+package com.caogen.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-06-30 11:03
+ */
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class SponsorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class, args);
+    }
+
+}
