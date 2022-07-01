daaa80af48a1ffd65a8376ac4081e188731c54af
康良玉 <kangly@vigamemedia.com>
2022-07-01 14:56:55 +0800

    新增广告搜索模块

---
+++ caogen-ad-service/ad-search/src/main/java/com/caogen/ad/SearchApplication.java
@@ -0,0 +1,30 @@
+package com.caogen.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-06-30 11:03
+ */
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableHystrix
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+
+}
