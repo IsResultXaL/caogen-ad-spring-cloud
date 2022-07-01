e11ffee5cac71d4cafd51ac888f355df93a56f4f
康良玉 <kangly@vigamemedia.com>
2022-07-01 19:29:39 +0800

    表数据导出到文件

--- caogen-ad-service/ad-common/src/main/java/com/caogen/ad/dump/DConstant.java
+++ caogen-ad-service/ad-common/src/main/java/com/caogen/ad/dump/DConstant.java
@@ -7,7 +7,7 @@ package com.caogen.ad.dump;
  */
 public class DConstant {
 
-    public static final String DATA_ROOT_DIR = "/Users/kcaogen/Downloads/";
+    public static final String DATA_ROOT_DIR = "/Users/kcaogen/Downloads/data/";
 
     // 各个表数据的存储文件名
     public static final String AD_PLAN = "ad_plan.data";
