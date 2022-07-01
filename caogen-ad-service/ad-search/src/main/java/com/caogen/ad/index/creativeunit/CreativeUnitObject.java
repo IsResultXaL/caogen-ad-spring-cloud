55cb0e92c96800e5f5d114281ed2f7e288430033
康良玉 <kangly@vigamemedia.com>
2022-07-01 17:39:48 +0800

    推广单元与创意关联索引定义

---
+++ caogen-ad-service/ad-search/src/main/java/com/caogen/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,21 @@
+package com.caogen.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-07-01 17:00
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+
+    private Long adId;
+
+    private Long unitId;
+
+}
