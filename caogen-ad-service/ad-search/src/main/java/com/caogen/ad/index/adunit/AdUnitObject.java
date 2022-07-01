bbe6c20ecbfb594edf181ab8a82d9c4e88dbee7c
康良玉 <kangly@vigamemedia.com>
2022-07-01 16:58:59 +0800

    推广单元索引定义

---
+++ caogen-ad-service/ad-search/src/main/java/com/caogen/ad/index/adunit/AdUnitObject.java
@@ -0,0 +1,51 @@
+package com.caogen.ad.index.adunit;
+
+import com.caogen.ad.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-07-01 16:54
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+
+    private Integer unitStatus;
+
+    private Integer positionType;
+
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject) {
+
+        if (null != newObject.getUnitId()) {
+            this.unitId = newObject.getUnitId();
+        }
+
+        if (null != newObject.getUnitStatus()) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+
+        if (null != newObject.getPositionType()) {
+            this.positionType = newObject.getPositionType();
+        }
+
+        if (null != planId) {
+            this.planId = newObject.getPlanId();
+        }
+
+        if (null != newObject.getAdPlanObject()) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+
+}
