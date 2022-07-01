6de5a0bc16baab91353cd1ff9b138828dcc174b4
康良玉 <kangly@vigamemedia.com>
2022-07-01 17:21:28 +0800

    兴趣索引定义

---
+++ caogen-ad-service/ad-search/src/main/java/com/caogen/ad/index/interest/UnitItObject.java
@@ -0,0 +1,21 @@
+package com.caogen.ad.index.interest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-07-01 17:18
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitItObject {
+
+    private Long unitId;
+
+    private String itTag;
+
+}
