1b12efcc7feef77b901d99a0ce8830022d9da4ae
康良玉 <kangly@vigamemedia.com>
2022-07-01 19:59:46 +0800

    索引操作的实现

---
+++ caogen-ad-service/ad-search/src/main/java/com/caogen/ad/mysql/constant/OpType.java
@@ -0,0 +1,15 @@
+package com.caogen.ad.mysql.constant;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-07-01 19:35
+ */
+public enum OpType {
+
+    ADD,
+    UPDATE,
+    DELETE,
+    OTHER;
+
+}
--- caogen-ad-service/ad-search/src/main/java/com/caogen/ad/utils/CommonUtils.java
+++ caogen-ad-service/ad-search/src/main/java/com/caogen/ad/utils/CommonUtils.java
@@ -14,4 +14,14 @@ public class CommonUtils {
         return map.computeIfAbsent(key, k -> factory.get());
     }
 
+    public static String stringConcat(String... strs) {
+        StringBuilder result = new StringBuilder();
+        for (String str : strs) {
+            result.append(str);
+            result.append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
 }
