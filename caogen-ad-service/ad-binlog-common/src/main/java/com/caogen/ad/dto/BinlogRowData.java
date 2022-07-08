6f17fa131e042cfcff2cc58f60a52d4e20fb8fdd
康良玉 <kangly@vigamemedia.com>
2022-07-08 10:32:56 +0800

    服务拆分 实现ad-binlog-common模块

---
+++ caogen-ad-service/ad-binlog-common/src/main/java/com/caogen/ad/dto/BinlogRowData.java
@@ -0,0 +1,25 @@
+package com.caogen.ad.dto;
+
+import com.github.shyiko.mysql.binlog.event.EventType;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-07-03 18:17
+ */
+@Data
+public class BinlogRowData {
+
+    private TableTemplate table;
+
+    private EventType eventType;
+
+    private List<Map<String, String>> after;
+
+    private List<Map<String, String>> before;
+
+}
---
+++ caogen-ad-service/ad-binlog-common/src/main/java/com/caogen/ad/dto/Template.java
@@ -0,0 +1,23 @@
+package com.caogen.ad.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @Author 康良玉
+ * @Description 描述
+ * @Create 2022-07-03 17:46
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Template {
+
+    private String database;
+
+    private List<JsonTable> tableList;
+
+}
